faed4eca819f7746faf1bd28d17d46ee82fdc57e
Will <wwelker@gmail.com>
2019-06-20 22:44:59 -0700

    autosync youtube-dl

--- ubuntu-setup/inspiron-5577.sh
+++ ubuntu-setup/inspiron-5577.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# setup script for my Insporon 5577 laptop
-# prior to using this, I could not get pas the sign in screen due to Nvidia graphic drivers.
-# at sign-in screen, I used CTRL-F2 to get command line then:
+apt install -y arduino
+apt install -y ntfs-config
+apt install -y exfat-fuse exfat-utils
+apt install -y net-tools
+apt install -y openssh-server
+apt install -y vlc
+apt install -y chromium-browser
 
-# sudo add-apt-repository ppa:graphics-drivers
-# sudo apt-get update
-# sudo apt-get upgrade
-# ubuntu-drivers-list
-# I was only able to get the 390 to load
-# sudo apt-install nvidia-driver-390
-# reboot (it froze when rebooting, had to hard reset)
-# No I could log in normally
-# Installed git so I could clone my script from github
-# sudo apt install git
-
-
-
+# get latest youtube-dl
+mkdir -pv /usr/local/bin
+wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
