4d88c246f3293dda37b22d7801c324fbcb54bb20
Will Welker <wwelker@gmail.com>
2019-07-19 18:52:48 -0700

    Create pi-setup.sh
    Set up the Pi and purge unused programs

---
+++ pi-setup/pi-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# run with sudo
+# Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre)
+apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre -y
+
+# Autoremove
+apt-get autoremove -y
+
+# Clean
+apt-get autoclean -y
+
+# Update
+apt-get update
+
+
+# -y option means it won't promt for Y/N to do the install
+apt-get install -y arduino
+apt-get install -y xscreensaver
+apt-get install -y motion
+apt-get install -y espeak
