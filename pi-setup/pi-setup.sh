1afeee6d96f4c9982527cf62dd8a79cd7374f2e8
WillWelker <wwelker@gmail.com>
2020-01-07 13:41:16 -0800

    autosync pi setup script update

--- pi-setup/pi-setup.sh
+++ pi-setup/pi-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run with sudo
 # Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre)
-apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre -y
+sudo apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre sense-emu-tools python3-sense-emu python-sense-emu python3-thonny smartsim bluej nuscratch sc -y
 
 # Autoremove
 apt-get autoremove -y
--- ubuntu-setup/inspiron-5577.sh
+++ ubuntu-setup/inspiron-5577.sh
@@ -13,3 +13,7 @@ apt install -y w3m
 mkdir -pv /usr/local/bin
 wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 chmod a+rx /usr/local/bin/youtube-dl
+
+This is Will 
+:o)
+
